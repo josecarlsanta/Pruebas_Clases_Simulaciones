f7f964178cc46d0a893154a1c35bfaa924a5f799
josecarlsanta <josecarlsanta@gmail.com>
2026-02-23 09:56:42 -0500

    deploy-github-pages-setup

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      base: '/Pruebas_Clases_Simulaciones/',
       server: {
         port: 3000,
         host: '0.0.0.0',
